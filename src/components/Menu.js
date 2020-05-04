86365086c3a8f12a48ba000f25b86fe408d2b6ca
Python-Development <legromant@gmail.com>
2020-05-04 23:33:18 +0300

    media queries

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,28 +1,19 @@
 import React from "react";
+import $ from "jquery";
 import {Navbar, Nav} from "react-bootstrap";
 import logo from "../image/nunox_logo.png";
 
 export default function Menu() {
+
+    $(document).on("click", "nav .nav-item", function(e) {
+        e.preventDefault();
+        let id  = $(this).attr('href');
+        let top = $(id).offset().top; // отримуємо координати блока
+        $('body, html').animate({scrollTop: top}, 800); // плавно переходимо до блоку
+    });
+
     return(
         <div id="home">
-            {/*<nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">*/}
-            {/*    <a href="#!" className="navbar-brand"><img src={logo} alt="nunox-logo"/></a>*/}
-            {/*    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">*/}
-            {/*        <span className="navbar-toggler-icon"></span>*/}
-            {/*    </button>*/}
-            {/*    <div className="collapse navbar-collapse" id="navbarResponsive">*/}
-            {/*        <ul className="navbar-nav ml-auto">*/}
-            {/*            <li className="nav-item"><a href="#home" className="nav-link">home</a></li>*/}
-            {/*            <li className="nav-item"><a href="#course" className="nav-link">course</a></li>*/}
-            {/*            <li className="nav-item"><a href="#feature" className="nav-link">feature</a></li>*/}
-            {/*            <li className="nav-item"><a href="#resources" className="nav-link">resources</a></li>*/}
-            {/*            <li className="nav-item"><a href="#clients" className="nav-link">clients</a></li>*/}
-            {/*            <li className="nav-item"><a href="#contact" className="nav-link">contact</a></li>*/}
-            {/*        </ul>*/}
-            {/*    </div>*/}
-            {/*</nav>*/}
-
-
             <Navbar bg="dark" variant="dark" expand="md" fixed="top">
                 <Navbar.Brand href="#home"><img src={logo} alt="nunox-logo"/></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
